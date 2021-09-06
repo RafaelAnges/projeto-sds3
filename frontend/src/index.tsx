e554748f677d2017b381ba7359d6a366315fbe81
Rafael santos <rafael_16001@hotmail.com>
2021-09-06 11:00:51 -0300

    Bootstrap

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import './assets/css/styles.css';
 import App from './App';
 
 ReactDOM.render(
@@ -8,4 +10,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
 
